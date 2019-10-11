27d53051ad1e32dd62a57724b0721b61d0eed379
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-11 13:51:59 +0200

    primer commit

---
+++ resumenproyecto/Resumenproyecto.java
@@ -0,0 +1,15 @@
+package resumenproyecto;
+
+
+public class Resumenproyecto {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        System.out.println("Hola mundo");
+    
+    
+    }
+    
+}
